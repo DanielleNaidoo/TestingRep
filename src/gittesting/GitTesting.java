b670b97740ad42f52733c51a85595b212a66e709
Stargate <Stargate@DESKTOP-1II2A3I.mshome.net>
2025-04-09 09:52:20 +0200

    First commit

---
+++ src/gittesting/GitTesting.java
@@ -0,0 +1,10 @@
+
+package gittesting;
+
+public class GitTesting {
+    public static void main(String[] args) {
+       System.out.print("Hello world");
+       System.out.print("Hello world");
+    }
+    
+}
